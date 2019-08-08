ba42d9421c200189624c418890e165846a7f5567
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-08-08 15:31:47 -0400

    solution Exercise 6

---
+++ Exercise 6/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <Header />
+        <hr>
+        <HomePageMain />
+        <hr>
+        <Footer />
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+    import HomePageMain from './components/HomePageMain.vue';
+
+    export default {
+
+        components: {
+            Header,
+            Footer,
+            HomePageMain
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Exercise 6/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <p>All Servers are managed here</p>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
